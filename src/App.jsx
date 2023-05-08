96478a4dd79b5fa23f19f6d408f796d2539579b5
Ashraful islam <theakashsarkar7@gmail.com>
2023-05-08 14:57:45 +0600

    calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function Board()
   const [square, setSquare] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-      if (square[i])
+      if (square[i] || calculateWinner(square))
       {
           return;
       }
@@ -31,8 +31,17 @@ export default function Board()
       setSquare(nextSquares);
       setXIsNext(!xIsNext);
   }
+    const winner = calculateWinner(square);
+    let status
+    if (winner)
+    {
+        status = "Winner " + winner;
+    } else {
+        status = "Next player " + (xIsNext ? "x" : "0");
+    }
   return (
     <>
+        <div className="status">{status}</div>
       <div className="board-row">
           <Square value={square[0]} onSquareClick={() => handleClick(0)}/>
           <Square value={square[1]} onSquareClick={() => handleClick(1)}/>
@@ -51,4 +60,25 @@ export default function Board()
 
     </>
   )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
