20dd9c5d855ea636315210053f40a47533f2340b
Ashraful islam <theakashsarkar7@gmail.com>
2023-05-09 17:26:38 +0600

    filter product table

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,84 @@
-import { useState } from "react";
-
-function Square({ value, onSquareClick }) {
-
-  return ( 
-      <button className="square"
-      onClick={onSquareClick}
-      >
-          {value}
-      </button>
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
     )
 }
 
-export default function Board()
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name : <span style={{ color: 'red' }}>{ product.name}</span>;
+    return (
+        <tr>
+            <td>{ name }</td>
+            <td>{ product.price }</td>
+        </tr>
+    );
+}
+
+function ProductTable({ products }) {
+    const rows = [];
+    let lastCategory;
+    products.forEach((products) => {
+        if (products.category !== lastCategory)
+        {
+            rows.push(
+                <ProductCategoryRow category={products.category} key={products.category} />
+            );
+        }
+        rows.push(
+            <ProductRow product={products} key={products.name} />
+        )
+        lastCategory = products.category;
+    });
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <label>
+                <input type="checkbox" />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+function FilterableProductTable({ products })
 {
-    const [xIsNext, setXIsNext] = useState(true);
-  const [square, setSquare] = useState(Array(9).fill(null));
+    return (
+        <>
+            <SearchBar />
+            <ProductTable products={products}/>
+        </>
+    );
+}
 
-  function handleClick(i) {
-      if (square[i])
-      {
-          return;
-      }
-      const nextSquares = square.slice();
-      if (xIsNext)
-      {
-          nextSquares[i] = 'x';
-      } else {
-          nextSquares[i] = '0'
-      }
-      setSquare(nextSquares);
-      setXIsNext(!xIsNext);
-  }
-  return (
-    <>
-      <div className="board-row">
-          <Square value={square[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={square[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={square[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-          <Square value={square[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={square[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={square[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-          <Square value={square[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={square[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={square[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
 
-    </>
-  )
+
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
 }
